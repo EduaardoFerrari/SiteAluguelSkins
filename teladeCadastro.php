2cab0d0c63888feab417406d3c98b0a33258954e
enzoqta <enzodalla2@gmail.com>
2025-06-04 20:04:59 -0300

    Tela de cadastro + css

---
+++ teladeCadastro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css.css">
+    <title>Cadastro Cliente</title>
+</head>
+
+<body>
+    <section class="header">
+        <section class="logo">
+            <img src="logo.png" alt="logo xhopi">
+            <h1> Xhopii </h1>
+        </section>
+        <a class="sair" href="#"> Sair </a>
+    </section>
+    <section class="navbar">
+        <a href="#"> Home </a>
+        <a href="#"> Cadastro Cliente </a>
+        <a href="#"> Cadastro Funcionário </a>
+        <a href="#"> Cadastro Produto </a>
+        <a href="#"> Ver Clientes </a>
+        <a href="#"> Ver Funcionários </a>
+        <a href="#"> Ver Produtos </a>
+    </section>
+    <section class="cadastro-maior">
+        <section class="Cadastro">
+            <h2>Cadastro Cliente</h2>
+            <input class="inputs" type="text" placeholder="Nome">
+            <input class="inputs" type="text" placeholder="Sobrenome">
+            <input class="inputs" type="text" placeholder="CPF">
+            <input class="inputs" type="date" required>
+            <input class="inputs" type="tel" placeholder="Telefone">
+            <input class="inputs" type="email" placeholder="Email">
+            <input class="inputs" type="password" placeholder="Senha">
+            <button>Cadastrar</button>
+        </section>
+    </section>
+</body>
+</html>
